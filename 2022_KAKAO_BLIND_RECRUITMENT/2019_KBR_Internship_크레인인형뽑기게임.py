265a6514463e2f451ba7a93b6380e50b2d4886c0
XronAce <xronace@gmail.com>
2022-02-10 13:58:43 +0900

    Removed unnecessary print()

--- 2022_KAKAO_BLIND_RECRUITMENT/2019_KBR_Internship_크레인인형뽑기게임.py
+++ 2022_KAKAO_BLIND_RECRUITMENT/2019_KBR_Internship_크레인인형뽑기게임.py
@@ -4,7 +4,6 @@ def solution(board, moves):
     answer = 0
 
     for move in moves:
-        print(f"move = {move}")
         for i in range(len(board)):
             if board[i][move-1] != 0:
                 stack.append(board[i][move-1])
