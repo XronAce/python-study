fda23e87e24d234fcce7050d879ffca17c8973ca
XronAce <xronace@gmail.com>
2022-02-14 11:11:55 +0900

    Commented out unnecessary lines

--- 2022_KAKAO_BLIND_RECRUITMENT/2019_KBR_오픈채팅방.py
+++ 2022_KAKAO_BLIND_RECRUITMENT/2019_KBR_오픈채팅방.py
@@ -19,5 +19,5 @@ def solution(record):
 
     return answer
 
-record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
-print(solution(record))
+# record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+# print(solution(record))
