0e8ef739bde3a97e22c425c3536bd012c777b67d
XronAce <xronace@gmail.com>
2022-03-08 19:00:14 +0900

    First commit. Need to work more on checking -1's

---
+++ Baekjoon/2839_설탕배달.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+if N == 4:
+    print(-1)
+elif N == 3:
+    print(1)
+else:
+    if N % 5 == 0:
+        print(N // 5)
+    elif N % 5 == 1:
+        print(N // 5 + 1)
+    elif N % 5 == 2:
+        print(N // 5 + 2)
+    elif N % 5 == 3:
+        print(N // 5 + 1)
+    else:
+        print(N // 5 + 2)
