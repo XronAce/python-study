855bc102837f144400e64113055348e7abdf97a6
XronAce <xronace@gmail.com>
2022-03-18 16:06:03 +0900

    Finalized with better efficiency.

--- Algorithm_with_Codeit/Binary_Search_Algorithm.py
+++ Algorithm_with_Codeit/Binary_Search_Algorithm.py
@@ -2,10 +2,8 @@ def binary_search(element, some_list):
     stidx = 0
     edidx = len(some_list)-1
     mdidx = (edidx+stidx)//2
-    flag = 0
-    count = 0
     
-    while flag == 0:
+    while True:
         if some_list[mdidx] == element:
             return mdidx
         elif some_list[mdidx+1] == element:
@@ -13,16 +11,21 @@ def binary_search(element, some_list):
         elif some_list[mdidx] > element:
             edidx = mdidx
             mdidx = (edidx+stidx)//2
-            count += 1
+            if mdidx == 0:
+                if some_list[mdidx] == element:
+                    return mdidx
+                else:
+                    return None
         else:
             stidx = mdidx
             mdidx = (edidx+stidx)//2
-            count += 1
-  
-
-        if count > 50:
-            break
-        
+            if mdidx == len(some_list)-2:
+                if some_list[mdidx] == element:
+                    return mdidx
+                elif some_list[mdidx+1] == element:
+                    return mdidx+1
+                else:
+                    return None
 
 print(binary_search(2, [2, 3, 5, 7, 11]))
 print(binary_search(0, [2, 3, 5, 7, 11]))
