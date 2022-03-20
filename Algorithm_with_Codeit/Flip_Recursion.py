93c4e9664ec2336bfd05c42db75c124d1f325cfc
XronAce <xronace@gmail.com>
2022-03-20 16:20:46 +0900

    codeit_algorithm study

---
+++ Algorithm_with_Codeit/Flip_Recursion.py
@@ -0,0 +1,9 @@
+# 파라미터 some_list를 거꾸로 뒤집는 함수
+def flip(some_list):
+    some_list = some_list[::-1]
+    return some_list
+
+# 테스트
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+some_list = flip(some_list)
+print(some_list)
