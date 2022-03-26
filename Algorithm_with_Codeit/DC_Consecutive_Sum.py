68232303b928f2b07d90b70de356292c0d88da2c
XronAce <xronace@gmail.com>
2022-03-26 10:46:16 +0900

    First commit. Need to simplify the base case

---
+++ Algorithm_with_Codeit/DC_Consecutive_Sum.py
@@ -0,0 +1,23 @@
+def consecutive_sum(start, end):
+    mid = (start + end) // 2
+    if end - start == 2:
+        return start + start + 1 + end
+    elif end - start == 1:
+        return start + end
+    else:
+        return consecutive_sum(start, mid) + consecutive_sum(mid+1, end)
+
+# 테스트
+print(consecutive_sum(1, 10))
+print(consecutive_sum(1, 100))
+print(consecutive_sum(1, 253))
+print(consecutive_sum(1, 388))
+
+"""
+Consecutive Sum in DP?
+Split the sum into half using recursive function. There must be the difference between recursive sum and consecutive sum.
+Divide and Conquer
+1. Divide
+2. Conquer
+3. Combine
+"""
