7f70b14190130dd2f682e130d9b3b8d7949bf1dc
XronAce <xronace@gmail.com>
2022-03-26 10:46:51 +0900

    Simplified base case.

--- Algorithm_with_Codeit/DC_Consecutive_Sum.py
+++ Algorithm_with_Codeit/DC_Consecutive_Sum.py
@@ -1,9 +1,7 @@
 def consecutive_sum(start, end):
     mid = (start + end) // 2
-    if end - start == 2:
-        return start + start + 1 + end
-    elif end - start == 1:
-        return start + end
+    if start == end:
+        return start
     else:
         return consecutive_sum(start, mid) + consecutive_sum(mid+1, end)
 
